b514fa49cece78520241ee1e0d4e2609b10f6499
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-12-11 01:03:31 +0300

    Set base url

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  'https://b795b019-1f84-41f4-93a3-a702d686c75a.mock.pstmn.io';
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './store.ts';
-import App from './App.tsx';
+import { store } from 'src/store.ts';
+import App from 'src/App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
