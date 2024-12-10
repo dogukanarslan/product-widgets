71b5c11d2b670cc965c51ad8fc3000155f146c15
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-12-11 01:02:05 +0300

    Add font family and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    fontFamily: {
+      body: ['Cabin'],
+    },
+    extend: {
+      colors: {
+        gray: '#212121',
+        lightgray: '#afc6bd',
+        darkgray: '#b0b0b0',
+        blue: '#2e3a8c',
+        green: '#3b755f',
+        beige: '#f2ebdb',
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
