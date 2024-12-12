55ef8aa14020510b82558d90aace53355815e281
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-12-12 20:24:59 +0300

    Updates

---
+++ .storybook/customTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  fontBase: 'Cabin, sans-serif',
+  brandImage:
+    'https://cdn.prod.website-files.com/611391a1477389e3857d8014/64cb3773a1ba8a68d73a88ad_greenspark-logo.svg',
+});
